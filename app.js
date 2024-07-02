79a33025c62a622f2a128aaac7cbedbd7ec1a5a8
pilgs <pedrenriquecunha@gmail.com>
2024-07-02 04:11:23 -0300

    Final Challenge ONE - Phase 2

---
+++ app.js
@@ -0,0 +1,91 @@
+// Display output texts
+function displayText(tag, text) 
+{
+    let userOutput = document.querySelector(tag);
+    userOutput.innerHTML = text;       
+}
+
+// Transition to the second part where the input had already been entered
+function secondScreen()
+{
+    displayText("h3", "");
+    displayText("h4", "");
+    document.getElementById("lupe-cat").style.display = "none";
+    let copyButton = document.getElementById("copyButton");
+    copyButton.style.visibility = "visible";
+    copyButton.style.pointerEvents = "auto";
+    document.getElementById("messageToCopy").style.visibility = "visible"
+}
+
+// Encrypt the user input text 
+function encryptText() 
+{
+    let inputText = document.querySelector("textarea").value;
+
+    // Check if the user didn´t input anything yet or had only inputted spaces
+    if (inputText.trim() != "")
+        {
+            // Regex to Encrypt
+            let encryptedText = removeDiacritics(inputText.toLowerCase())
+                                .replace(/e/g, "enter")
+                                .replace(/i/g, "imes")
+                                .replace(/a/g, "ai")
+                                .replace(/o/g, "ober")
+                                .replace(/u/g, "ufat");
+            
+            displayText("h2", encryptedText);
+            secondScreen();
+            document.querySelector("textarea").value = ""; 
+        }
+    
+    // Error message
+    else
+    {
+        alert("Error #01:\nPlease, enter a message to be encrypted first.")
+        return 1;
+    }
+}
+
+// Decrypt the encrypted text
+function decryptText() 
+{
+    let textToDecrypt = document.querySelector("textarea").value;
+    
+    // Check if the user didn't encrypt anything yet
+    if (textToDecrypt.trim() != "")
+        {
+            let decryptedText = removeDiacritics(textToDecrypt.toLowerCase())
+                                .replace(/enter/g, "e")
+                                .replace(/imes/g, "i")
+                                .replace(/ai/g, "a")
+                                .replace(/ober/g, "o")
+                                .replace(/ufat/g, "u");
+            displayText("h2", decryptedText);
+            document.querySelector("textarea").value = "";
+        }
+
+    // Error message
+    else
+    {
+        alert("Error #02:\nPlease, first you need to enter a message to be encrypted.");
+        return 2;
+    }
+}
+
+// Remove diacritcs
+function removeDiacritics(userInput)
+{
+    return userInput.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+// Copy the outputted text to clipboard
+function copyText() 
+{
+    let textToClipboard = document.getElementById("messageToCopy").innerText;
+    if (textToClipboard != "")
+        {
+            navigator.clipboard.writeText(textToClipboard);
+            document.getElementById("messageToCopy").innerText = "";
+        }
+
+}
